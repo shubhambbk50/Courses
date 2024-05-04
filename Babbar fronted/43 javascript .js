a2739168889ed411de64d6d87faecd7ce01e8078
Shubham Verma <94617569+shubhambbk50@users.noreply.github.com>
2024-05-04 11:14:40 +0000

    commit

--- Babbar fronted/43 javascript .js
+++ Babbar fronted/43 javascript .js
@@ -9,6 +9,45 @@
 // Java: Java is widely used in enterprise environments, especially for building large-scale applications and systems. It is also popular for Android app development.
 // JavaScript: JavaScript is primarily used for web development, powering the interactivity of websites and web applications. It is also increasingly used for server-side development (with Node.js) and in various other contexts beyond the web.
 
-Scope:
-var: Variables declared with var are function-scoped or globally scoped. They are accessible throughout the entire function in which they are defined, regardless of block scope.
-let: Variables declared with let are block-scoped. They are only accessible within the block (enclosed by curly braces) in which they are defined.
+--------------------------types of variables
+// var: Variables declared with var are function-scoped or globally scoped. They are accessible throughout the entire function in which they are defined, regardless of block scope.
+// var: Allows re-declaration of variables within the same scope. 
+// let: Variables declared with let are block-scoped. They are only accessible within the block (enclosed by curly braces) in which they are defined.
+// let: Does not allow re-declaration of variables within the same scope. 
+
+// const: unchangeable value
+
+---------------------------Primitive data types
+// String - sequence of Characters
+// Number - integers, floating
+// Boolean -
+// undefined - undefined represents a variable that has been declared but has not been assigned a value.
+//             let x;
+//             console.log(x); // Output: undefined
+// null -      It is often used to explicitly indicate that a variable does not have a meaningful value.
+//             Unlike undefined, null is explicitly assigned to a variable to signify that it has no value.
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
